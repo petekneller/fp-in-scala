71a575302b24d930559e8f386f6343af11960a44
Pete <peter.kneller@db.com>
2014-10-07 10:30:57 +0100

    finished functional data structures examples

--- src/test/scala/chapter3_4/ListFoldTests.scala
+++ src/test/scala/chapter3_4/ListFoldTests.scala
@@ -1,8 +1,6 @@
 package chapter3_4
 
 import org.scalatest.FunSuite
-import sun.reflect.generics.reflectiveObjects.NotImplementedException
-
 import scala.annotation.tailrec
 
 class ListFoldTests extends FunSuite {
@@ -50,8 +48,17 @@ class ListFoldTests extends FunSuite {
 
   def sumLeft(l: List[Int]): Int = foldLeft(l, 0)(_ + _)
 
+  def flatten[A](as: List[List[A]]): List[A] = {
+    foldRight(as, Nil: List[A])((subList, out) => foldRight(subList, out)((a, out) => Cons(a, out)))
+  }
+
+  def map[A, B](as: List[A])(f: A => B): List[B] = foldRight(as, Nil: List[B])((a, bs) => Cons(f(a), bs))
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = flatten(map(as)(f))
+
   val testList = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
   val revTestList = Cons(4, Cons(3, Cons(2, Cons(1, Nil))))
+  val twoTestLists = Cons(1, Cons(2, Cons(3, Cons(4, Cons(1, Cons(2, Cons(3, Cons(4, Nil))))))))
 
   test("reverse") {
 
@@ -111,7 +118,46 @@ class ListFoldTests extends FunSuite {
     }
 
     assert(foldRight3(testList, Nil: List[Int])(Cons.apply _) === testList)
+  }
 
+  test("exercise 3.14") {
+    def append2[A](as: List[A], a: A): List[A] = foldRight(as, Cons(a, Nil))(Cons.apply _)
+
+    assert(append2(testList, 5) === Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, Nil))))))
+  }
+
+  test("exercise 3.15") {
+
+    assert(flatten(Cons(testList, Cons(testList, Nil))) === twoTestLists)
+  }
+
+  test("exercise 3.20") {
+
+    val as = Cons(1, Cons(2, Nil))
+    assert(flatMap(as)(a => testList) === twoTestLists)
+  }
+
+  test("exercise 3.21") {
+
+    def filter2[A](as: List[A])(f: A => Boolean): List[A] = flatMap(as)((a) => if (f(a)) Cons(a, Nil) else Nil)
+
+    assert(filter2(testList)(_ % 2 == 0) === Cons(2, Cons(4, Nil)))
+  }
+
+  ignore("exercise 3.24") {
+
+    def hasSubsequence[A](as: List[A], sub: List[A]): Boolean = {
+//      val potentiallyMatching = dropWhile(as, _ != head(sub))
+//      val matching = every(zipWith(potentiallyMatching, sub, _ == _))
+//      if (matching)
+//        true
+//      else
+//        hasSubsequence(tail(potentiallyMatching), sub)
+      false // too lazy to implement all the util fns
+    }
+
+    assert(hasSubsequence(testList, Cons(2, Cons(3, Nil))) === true)
+    assert(hasSubsequence(testList, Cons(3, Cons(2, Nil))) === false)
   }
 
 
