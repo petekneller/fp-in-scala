f56e8accfe14bde65fe31ace4966b51c571c4d76
Pete Kneller <Peter.Kneller@barclayswealth.com>
2016-05-17 21:23:52 +0100

    My formulation for Trampolining of function calls. Now to add the proper formulation

--- src/test/scala/chapter13/MyTrampolineTests.scala
+++ src/test/scala/chapter13/MyTrampolineTests.scala
@@ -4,40 +4,88 @@ import org.scalatest.{Matchers, FunSuite}
 
 class MyTrampolineTests extends FunSuite with Matchers {
 
-//  case class Fn[A](a: A => A) {
-//    def flatMap(f: A => Fn[A]): Fn[A] = Fn(f(a))
-//    def map(f: A => A): Fn[A] = Fn(f(a))
-//  }
+  val f1 = (a: Int) => a * 2
+  val f2 = (a: Int) => f1(a) + 4
+  val f3 = (a: Int, b: Int) => f1(a) + f2(b)
 
   test("too much function composition blows the stack") {
 
-    val f1 = (a: Int) => a
-    val f1s = List.fill(1000000)(f1)
-    val f2 = f1s.fold(f1)(_ compose _)
-//    f2(2) should equal(2)
 
-//    val a = Fn((a: Int) => a)
-//    val f3s = List.fill(1000)(a)
-//    val f4 = f3s.foldLeft[Fn[List[Int => Int]]](Fn(List.empty[Int => Int])){
-//      (acc, i) =>
-//        for {
-//          as <- acc
-//          a <- i
-//        } yield as compose a
-//    }
-//    f4()
+    val f1s = List.fill(1000000)(f1)
+    val fx = f1s.fold(f1)(_ compose _)
+
+    //this will blow the stack
+//    fx(2) should equal(2)
   }
 
-  test("function composition can be trampolined") {
+  // A first crack at turning function calls into interpreters
+  // ---------------------------------------------------------
 
-    trait Tramp[A]
-    case class Return[A](a: A) extends Tramp[A]
-    case class Call[A, B](a: A, f: A => Tramp[B]) extends Tramp[A]
+  test("just returning values") {
+    case class Return[A](v: A)
 
-    val f = (a: Int) => Return(a)
-    def compose[A, B, C](f1: A => B, f2: B => C): A => C = (a: Int) => f2(f1(a))
+    def run[A](t: Return[A]): A = t.v
 
-    f(2).a should equal(2)
+    // a function that returns a value
+    val fx = (a: Int) => Return(a)
+    run(fx(2)) should equal(2)
+  }
+
+  test("calling legacy functions") {
+    case class Call[A, B](f: A => B, v: A)
+
+    def run[A, B](t: Call[A, B]): B = t.f(t.v)
+
+    // a function that requests the interpreter to call a legacy function and returns that value
+    val fx = (a: Int) => Call(f1, a)
+    run(fx(2)) should equal(4)
+  }
+
+  // So that last interpreter can handle function calls only in the tail position.
+  // What about where there is something to be done post- function call?
+
+  test("supplying a continuation") {
+    case class Call[A, B, C](f: A => B, v: A, cont: B => C)
+
+    def run[A, B, C](t: Call[A, B, C]): C = t.cont(t.f(t.v))
+
+    val fx = (a: Int) => Call(f1, a, (b: Int) => b + 1)
+    run(fx(2)) should equal(5)
+  }
+
+  test("executing a side-effect") {
+    case class Return[A](cont: () => A)
+
+    def run[A](t: Return[A]): A = t.cont()
+
+    val fx = (a: Int) => Return{ () => println("fooey"); 2 }
+    run(fx(1)) should equal(2)
+  }
+
+  test("call a function and then return a value, after executing a side effect") {
+    case class Return[A](cont: () => A)
+    case class Call[A, B, C](f: A => B, v: A, cont: B => Return[C])
+
+    def run[A, B, C](f: Call[A, B, C]): C = f.cont(f.f(f.v)).cont()
+
+    val fx = (a: Int) => Call(f1, a, (b: Int) => Return{ () => println(b); b + 1 })
+    run(fx(2)) should equal(5)
+  }
+
+  test("arbitrary depth function calls") {
+    trait Tramp[+A, +B, C]
+    case class Return[A](cont: () => A) extends Tramp[Nothing, Nothing, A]
+    case class Call[A, B, C](f: A => B, v: A, cont: B => Tramp[_, _, C]) extends Tramp[A, B, C]
+
+    def run[A, B, C](t: Tramp[A, B, C]): C = {
+      t match {
+        case Return(cont) => cont()
+        case Call(f, v, cont) => run(cont(f(v)))
+      }
+    }
+
+    val t = Call(f1, 2, (b: Int) => Call(f2, b, (c: Int) => Return(() => c + 1)))
+    run(t) should equal(13)
   }
 
 }
