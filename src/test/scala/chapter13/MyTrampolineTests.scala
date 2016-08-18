34a48a749952570e9fc29411d5645e51095beea7
Pete Kneller <Peter.Kneller@barclayswealth.com>
2016-08-18 11:43:53 +0100

    Add example of doing trampolining using exceptions, from the book

--- src/test/scala/chapter13/MyTrampolineTests.scala
+++ src/test/scala/chapter13/MyTrampolineTests.scala
@@ -208,7 +208,7 @@ class MyTrampolineTests extends FunSuite with Matchers {
 
     def flatMap[A, B](t: Tramp[A], f: A => Tramp[B]): Tramp[B] = Call(t, f)
 
-    @annotation.tailrec
+    //@annotation.tailrec
     def run[A, B, C](tramp: Tramp[A]): A = tramp match {
       case Return(a) => a
       case Cont(cont) => cont()
@@ -219,5 +219,74 @@ class MyTrampolineTests extends FunSuite with Matchers {
       }
     }
   }
+
+  // Or, an alternate impl of stack safety using Exceptions.
+  // Taken from the FP in Scala code, file Throw.scala
+  object UsingExceptions {
+    /**
+      * A version of `TailRec` implemented using exceptions.
+      * In the implementation of `flatMap`, rather than calling
+      * the function, we throw an exception indicating what
+      * function we want to call. A central loop repeatedly tries
+      * and catches these exceptions to force the computation.
+      */
+    trait Throw[+A] {
+      import Throw._
+
+      @annotation.tailrec
+      final def run: A = this match {
+        case Done(a) => a
+        case More(thunk) => force(thunk).run
+      }
+    }
+
+    object Throw {
+
+      /* Exception indicating that the central loop should call `f(a)`. */
+      case class Call[A,+B] private[Throw] (a: A, f: A => B) extends Exception {
+        override def fillInStackTrace = this
+      }
+
+      case class Done[+A](a: A) extends Throw[A]
+      case class More[+A](thunk: () => Throw[A]) extends Throw[A]
+
+      /* Defer evaluation of `f(a)` to the central evaluation loop. */
+      def defer[A,B](a: A)(f: A => B): B =
+        throw new Call(a, f)
+
+      /* Central evaluation loop. */
+      def ap[A,B](a: A)(f: A => B): B = {
+        var ai: Any = a
+        var fi: Any => Any = f.asInstanceOf[Any => Any]
+        while (true) {
+          try return fi(ai).asInstanceOf[B]
+          catch { case Call(a2,f2) => ai = a2; fi = f2; }
+        }
+        return null.asInstanceOf[B] // unreachable
+      }
+
+      /* Convenience function for forcing a thunk. */
+      def force[A](f: () => A): A =
+        ap(f)(f => f())
+
+      def more[A](a: => Throw[A]): Throw[A] = More(() => a)
+
+      /* `Throw` forms a `Monad`.
+
+      def unit[A](a: => A): Throw[A] = more(Done(a))
+
+      def flatMap[A,B](a: Throw[A])(f: A => Throw[B]): Throw[B] =
+        a match {
+          case Done(a) => f(a)
+          case More(thunk) =>
+            try thunk() flatMap f
+            catch { case Call(a0,g) => more {
+              defer(a0)(g.asInstanceOf[Any => Throw[A]].
+                andThen(_ flatMap f))
+            }}
+        }
+
+       */
+    }}
 
 }
